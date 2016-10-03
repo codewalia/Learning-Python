d907d19196e42cb522bcd36f3d147192b547664f
codewalia <ankurwalia@yahoo.in>
2016-10-03 16:31:27 +0530

    usinf formatter
    continue using formatter and operations on strings

---
+++ ex3.py
@@ -0,0 +1,8 @@
+#using formatter continue
+
+formatter = "%r %r %r %r"
+
+print formatter % (1,2,3,4)
+print formatter % ("one","two","three","four")
+print formatter % (formatter,formatter,formatter,formatter)
+
