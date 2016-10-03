6bd5db6d65d05441f2a76a48c6ba2a68a310a4ce
codewalia <ankurwalia@yahoo.in>
2016-10-03 15:07:49 +0530

    using some basic maths
    using some basic maths

--- ex1.py
+++ ex1.py
@@ -1,4 +1,11 @@
-print "hello worl"
-print "kya chal rha hai"
-print "this is first python prog m writing"
-print "i hope it shud run"
+print "i will now count my chicekn"
+#simple o/p
+print "Hens",25 + 30/6
+#will print hens and in the same line it will print 25 + 5 = 30
+print "roosters",100- 25 * 3 % 4
+#it will print roosters with a number in the smae line
+
+print "now i will count the eggs"
+
+print 3 + 2 + 1 - 5 + 4 % 2 - 1/4 + 6
+
