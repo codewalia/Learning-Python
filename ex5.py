6d189dfb661caa4277e2ae85eb6be7bb7c256b86
codewalia <ankurwalia@yahoo.in>
2016-10-04 14:02:17 +0530

    using system module and argv

---
+++ ex5.py
@@ -0,0 +1,19 @@
+#params, unpacking and vriables
+
+from sys import argv
+
+#using sys modules, argv is the arguments
+
+
+
+
+script , first, second, third = argv
+
+#unpacking the argument argv and splitting it into 4 variables
+
+print "the script is called ", script
+print "first variable is ", first
+print "second variable is ", second
+print "third variable is " , third
+
+
