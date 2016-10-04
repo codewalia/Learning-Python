62f10a1f71bab7154b5e32ad2ca39bcfe930ba6c
codewalia <ankurwalia@yahoo.in>
2016-10-04 13:39:41 +0530

    using user interface
    using raw_input fiunction

---
+++ ex4.py
@@ -0,0 +1,13 @@
+#using user inputs
+
+print "how old are you"
+age =raw_input()
+print "how tall are you"
+height = raw_input()
+print "how much do u weigh?"
+weight = raw_input()
+
+pincode = raw_input("what is the pincode of your area?")
+print pincode
+
+print "oh, so you are %r old, %r tall and %r heavy" % (age, height, weight)
